5f109e09cb228aed5a8b4b49521697dc9f3d2a0d
Michael Campbell <michael.campbell@simplybusiness.com>
2022-11-29 12:45:39 -0500

    02 powers of 2

---
+++ lib/02_powers_of_two.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class PowersOfTwo
+  def self.power_of_two(n)
+  end
+end
--- spec/02_powers_of_two_spec.rb
+++ spec/02_powers_of_two_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require '01_length'
+require '02_powers_of_two'
 
-describe 'calculates array lengths' do
+describe 'calculates powers of 2' do
   it 'runs a test successfully' do
     expect(true).to be_truthy
   end
-endend
+end
