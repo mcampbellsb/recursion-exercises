492f20d972967fe7488906394cabc2abdbbe49fd
Michael Campbell <michael.campbell@simplybusiness.com>
2022-11-29 12:09:36 -0500

    01_length

---
+++ lib/01_length.rb
@@ -0,0 +1,7 @@
+# Write a function that calculates the length of an array.
+
+class ArrayLength
+  def self.length(a)
+
+  end
+end
---
+++ spec/01_length_spec.rb
@@ -0,0 +1,7 @@
+require '01_length'
+
+describe "calculates array lengths" do
+  it "runs a test successfully" do
+    expect(true).to be_truthy
+  end
+end
