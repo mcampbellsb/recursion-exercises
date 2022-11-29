f19b9842feac5857fc64afef669fc7177a1c81aa
Michael Campbell <michael.campbell@simplybusiness.com>
2022-11-29 12:52:11 -0500

    reverse a string

---
+++ lib/reverse_a_string.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Write a function that will reverse a string.  eg: "recursion" => "noisrucer"
+
+class StringReverser
+  def self.reverse(a); end
+end
---
+++ spec/reverse_a_string_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'reverse_a_string'
+
+describe 'reverses a string' do
+  it 'runs a test successfully' do
+    expect(true).to be_truthy
+  end
+end
