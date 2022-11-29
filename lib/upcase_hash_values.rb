b34a212675aa9b0634fa2cb7ae114b8d081a0c6e
Michael Campbell <michael.campbell@simplybusiness.com>
2022-11-29 13:26:05 -0500

    upcase hash values

---
+++ lib/upcase_hash_values.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Assume: Type = hash<string, string|Type>
+# Write a function that given a Type will upcase all the string values of Type.
+# [optional] expand Type to be hash<string, string|Array<string|Type>|Type> (note: This
+# is *NOT* an array of string or an array of Type, it is an array that has string's or
+# Types, possibly mixed.)
+
+class TypeUpperCaser
+  def self.upcase_type(a); end
+end
---
+++ spec/upcase_hash_values_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'upcase_hash_values'
+
+describe 'upcases values of a Type type' do
+  it 'runs a test successfully' do
+    expect(true).to be_truthy
+  end
+end
