01ad983212d3d8d18f2ccf7f39099e108b4f424f
Michael Campbell <michael.campbell@simplybusiness.com>
2022-11-29 12:15:02 -0500

    Rubocop'd

--- lib/01_length.rb
+++ lib/01_length.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Write a function that calculates the length of an array.
 
 class ArrayLength
--- spec/01_length_spec.rb
+++ spec/01_length_spec.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 require '01_length'
 
 describe "calculates array lengths" do
   it "runs a test successfully" do
+describe 'calculates array lengths' do
+  it 'runs a test successfully' do
     expect(true).to be_truthy
   end
 end
