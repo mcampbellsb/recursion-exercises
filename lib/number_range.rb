a6bda6c19e73341964b30778e1d28501fd10accc
Michael Campbell <michael.campbell@simplybusiness.com>
2022-11-30 13:22:44 -0500

    number range

---
+++ lib/number_range.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+# write a function that takes (a, b) and generates an array of all the numbers a..b.
---
+++ spec/number_range_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'number_range'
+
+describe 'generates ranges' do
+  it 'runs a test successfully' do
+    expect(true).to be_truthy
+  end
+end
