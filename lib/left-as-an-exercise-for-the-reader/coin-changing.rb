6327e09b28555740748752c04485ae7306538c1a
Michael Campbell <michael.campbell@simplybusiness.com>
2022-12-01 18:37:10 -0500

    added coin changing

---
+++ lib/left-as-an-exercise-for-the-reader/coin-changing.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Write a function that calculates the number of ways to make an amount given a list of
+# denominations.
+# eg: how many ways can you make a dollar using standard US currency?
+# inputs: (100, [1, 5, 10, 25, 50])
+
+# This is a classic programming exercise.  See:
+# https://mitp-content-server.mit.edu/books/content/sectbyfn/books_pres_0/6515/sicp.zip/full-text/book/book-Z-H-11.html#%_sec_1.2.1
+# (search for "counting change")
+
+# Note: I find this exercise quite difficult, and have to look it up every time.  Even
+# when I do, I'm not sure I "get it" in terms of expressing this recursively.
--- lib/number_range.rb
+++ lib/number_range.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
 # write a function that takes (a, b) and generates an array of all the numbers a..b.
+# Your choice of b being inclusive or exclusive.  How might you do either?
